897df61ae7f56a12ffe2e721bbf6f610ec5f2013
hugosenari <hugosenari@gmail.com>
2022-03-23 22:56:49 -0300

    feat(nimpkgs): remove lock

---
+++ v1_3_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''ZeroMQ wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-zmq-v1_3_1.flake = false;
+  inputs.src-zmq-v1_3_1.ref   = "refs/tags/v1.3.1";
+  inputs.src-zmq-v1_3_1.owner = "nim-lang";
+  inputs.src-zmq-v1_3_1.repo  = "nim-zmq";
+  inputs.src-zmq-v1_3_1.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zmq-v1_3_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-zmq-v1_3_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
